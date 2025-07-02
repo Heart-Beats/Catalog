d192ce5e401a6ac5ba72cf4b6f5dde3d46c96fcf
张磊 <zhangl171@chinatelecom.cn>
2025-07-02 14:28:09 +0800

    发布脚本修改，改为发布至 MavenCentral

--- catalog/build.gradle.kts
+++ catalog/build.gradle.kts
@@ -1,40 +1,60 @@
 plugins {
 	id("version-catalog")
-	id("maven-publish")
 }
 
-apply {
-	from("../maven_publish.gradle")
+// 定义合并 toml 文件的任务
+tasks.register("mergeTomlFiles") {
+	doLast {
+		val inputFiles = listOf(
+			file("tomls/versionLibs.toml"),
+			file("tomls/pluginLibs.toml"),
+			file("tomls/dependendLibs.toml"),
+			file("tomls/bundleLibs.toml"),
+		)
+		val outputFile = rootProject.file("tomls/combinedLibs.toml")
+
+		if (!outputFile.exists()) {
+			// 确保创建的是文件的父目录，而不是文件本身
+			outputFile.parentFile?.mkdirs()
+		}
+
+		// 清空输出文件
+		outputFile.writeText("")
+
+		inputFiles.forEachIndexed { index, inputFile ->
+			if (inputFile.exists()) {
+				if (index < inputFiles.size) {
+					if (index > 0) {
+						outputFile.appendText("\n\n")
+					}
+
+					outputFile.appendText("##################################  ${inputFile.nameWithoutExtension}   #####################################\n")
+				}
+
+				// 将每个文件的内容追加到输出文件
+				outputFile.appendText(inputFile.readText())
+				// 每个文件内容后添加一个空行分隔
+				outputFile.appendText("\n")
+			}
+		}
+	}
+}
+
+// 确保在生成目录前合并 toml 文件并处理合并后的文件
+tasks.named("generateCatalogAsToml") {
+	dependsOn("mergeTomlFiles")
 }
 
 catalog {
 	// 定义目录
 	versionCatalog {
-		val files = files(
-			"/tomls/versionLibs.toml", "/tomls/pluginLibs.toml",
-			"/tomls/dependendLibs.toml", "/tomls/bundleLibs.toml",
-			"/tomls/compilerLibs.toml"
-		)
-
-		from(files)
+		// 确保文件存在且路径正确
+		val combinedFile = rootProject.file("tomls/combinedLibs.toml")
+		if (combinedFile.exists()) {
+			// 直接传入 File 对象
+			from(files(combinedFile))
+		} else {
+			logger.warn("Combined toml file does not exist: ${combinedFile.absolutePath}")
+		}
 	}
 }
-
-// project.publishing {
-// 	val publish_groupId: String by extra
-// 	println("publishing --------->${publish_groupId}")
-// 	publications {
-// 		this.register("versionCatalog", MavenPublication::class.java) {
-// 			this.groupId = "com.hl"
-// 			this.artifactId = "catalog"
-// 			this.version = "1.0.0"
-//
-// 			this.from(components["versionCatalog"])
-// 		}
-// 	}
-//
-// 	this.repositories {
-// 		// 配置 MavenLocal 仓库
-// 		// mavenLocal()
-// 	}
-// }
